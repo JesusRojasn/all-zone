adb9dfa4d8ef741f8cb4cd66497467d74653c3bf
Carayao <107008370+Carayao@users.noreply.github.com>
2023-04-03 21:49:46 -0400

    mejoramiento agregar publicacion
    se agrega boton para subir imagen, texto descripcion se expande.

--- src/Views/AgregarPublicacion.jsx
+++ src/Views/AgregarPublicacion.jsx
@@ -1,7 +1,8 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import Dropdown from 'react-bootstrap/Dropdown';
 import Figure from 'react-bootstrap/Figure';
+import Stack from 'react-bootstrap/Stack';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 function AgregarPublicacion() {
   return (
@@ -17,45 +18,63 @@ function AgregarPublicacion() {
             alt="171x180"
             src="https://www.lenovo.com/medias/lenovo-laptop-thinkpad-x1-carbon-gen-9-14-subseries-hero.png?context=bWFzdGVyfHJvb3R8MzM4MDg3fGltYWdlL3BuZ3xoMGYvaGY3LzE0MDcwNjc0NzE4NzUwLnBuZ3xiMDUwZmMyMGMxODkxY2JkNTdhMjZmMWM1YWY1NWZmYTllMTg4Y2E1NGFjMjMzMjRkZjQ1YjA1ZjA3YTFjMThi"
           />
-          <Figure.Caption>
-            Cargar Imagen
-          </Figure.Caption>
-        </Figure>
+          <Form.Group className="position-relative mb-3">
+            
+            <Form.Control
+              type="file"
+              required
+              name="file"
+            //onChange={handleChange}
+            //isInvalid={!!errors.file}
+            />
+            <Form.Control.Feedback type="invalid" tooltip>
 
-        <div className='nombresPublicar'>
-          <Form.Group className="caja" controlId="formGroupEmail">
-            <Form.Label>Nombre de producto</Form.Label>
-            <Form.Control type="text" placeholder="" />
+            </Form.Control.Feedback>
           </Form.Group>
 
-        </div>
 
-        <Form.Group className="caja" controlId="formGroupEmail">
-          <Form.Label>Descripción</Form.Label>
+
+        </Figure>
+
+
+        <Form.Group className="caja">
+          <Form.Label>Nombre de producto</Form.Label>
           <Form.Control type="text" placeholder="" />
         </Form.Group>
 
-        <Form.Group className="caja" controlId="formGroupPassword">
 
-          <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-              Categoría
-            </Dropdown.Toggle>
+        <Form.Group className="caja">
+          <Form.Label>Descripción</Form.Label>
+          <InputGroup>
 
-            <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1">Notebook</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Celulares</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Televisores</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
+            <Form.Control as="textarea" aria-label="With textarea" />
+          </InputGroup>
         </Form.Group>
 
-        <Form.Group className="caja" controlId="formGroupEmail">
+        <Form.Group className="caja">
+          <Form.Label>Categoría</Form.Label>
+          <Form.Select aria-label="Default select example">
+            <option>Selecciona categoría</option>
+            <option value="1">Notebooks</option>
+            <option value="2">Celulares</option>
+            <option value="3">Televisores</option>
+          </Form.Select>
+        </Form.Group>
+
+        <Form.Group className="caja">
           <Form.Label>Precio</Form.Label>
-          <Form.Control type="number" placeholder="XXXXX" />
+          <InputGroup className="mb-3">
+            <InputGroup.Text>$</InputGroup.Text>
+            <Form.Control aria-label="Amount (to the nearest dollar)" />
+            <InputGroup.Text></InputGroup.Text>
+          </InputGroup>
         </Form.Group>
+
         <div>
-          <Button className='boton'>Publicar</Button>{' '}
+          <Stack className='btn-publicar' direction="horizontal">
+            <Button variant="secondary">Save changes</Button>
+            <Button variant="outline-secondary">Cancel</Button>
+          </Stack>
         </div>
 
       </Form>
