bea3f616f0fed9e827a2e13925e3eadc6d44d18a
filipgonzo <109770351+filipgonzo@users.noreply.github.com>
2023-04-05 00:21:37 -0400

    consumo de Json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import InicioSesion from "./Views/InicioSesion";
@@ -13,27 +13,45 @@ import MarketPrivado from "./Views/MarketPrivado";
 import Usuario from "./Views/Usuario";
 import MisPublicaciones from "./Views/MisPublicaciones";
 import Detalle from "./Views/Detalle";
+import MyContext from "./contexts/MyContext.jsx";
 
 
 function App() {
+
+  const [productos, setProductos] = useState([]);
+
+  const getProductos = async () => {
+    const res = await fetch('http://localhost:3000/productos.json');
+    const data = await res.json();
+    setProductos(data);
+  };
+  console.log(productos);
+
+  useEffect(() => {
+    getProductos();
+  }, []);
+
   return (
     <div className="App">
-      <BrowserRouter>
-      <Logo></Logo>
-      
-        <Routes>       
-          <Route path="/" element={<MarketPublico></MarketPublico>}></Route>   
-          <Route path="/registro" element={<Registro></Registro>}></Route>
-          <Route path="/InicioSesion" element={<InicioSesion></InicioSesion>}></Route>
-          <Route path="/publicar" element={<AgregarPublicacion></AgregarPublicacion>}></Route>
-          <Route path="/MarketPrivado" element={<MarketPrivado></MarketPrivado>}></Route>
-          <Route path="/usuario" element={<Usuario></Usuario>}></Route>
-          <Route path="/MisPublicaciones" element={<MisPublicaciones></MisPublicaciones>}></Route>
-          <Route path="/Detalle" element={<Detalle></Detalle>}></Route>
-        </Routes>
-        
-        <Footer></Footer>
-      </BrowserRouter>
+      <MyContext.Provider value={{productos}}>
+        <BrowserRouter>
+        <Logo></Logo>
+
+          <Routes>       
+            <Route path="/" element={<MarketPublico></MarketPublico>}></Route>   
+            <Route path="/registro" element={<Registro></Registro>}></Route>
+            <Route path="/InicioSesion" element={<InicioSesion></InicioSesion>}></Route>
+            <Route path="/publicar" element={<AgregarPublicacion></AgregarPublicacion>}></Route>
+            <Route path="/MarketPrivado" element={<MarketPrivado></MarketPrivado>}></Route>
+            <Route path="/usuario" element={<Usuario></Usuario>}></Route>
+            <Route path="/MisPublicaciones" element={<MisPublicaciones></MisPublicaciones>}></Route>
+            <Route path="/Detalle" element={<Detalle></Detalle>}></Route>
+            <Route path="/detalle/:id" element={<Detalle></Detalle>}></Route>
+          </Routes>
+          
+          <Footer></Footer>
+        </BrowserRouter>
+      </MyContext.Provider>
     </div>
   );
 }
--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,39 +1,70 @@
 import { Container } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
-import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import MyContext from "../contexts/MyContext";
+import { useContext } from "react";
 
 function Cards() {
+  const {productos} = useContext(MyContext);
+  const navigate = useNavigate();
+
+  const irDetalle= () => {
+    navigate(`/detalle/${productos.ID}`)
+  }
+
+
+ 
   return (
-    <div>
-      <div className="cards">
-        <Container>
-          <Card
-            style={{
-              width: "18rem",
-              borderColor: "#7DCDEF",
-              borderWidth: "2px",
-            }}
-          >
-            <Card.Img
-              variant="top"
-              src="https://www.lenovo.com/medias/lenovo-laptop-thinkpad-x1-carbon-gen-9-14-subseries-hero.png?context=bWFzdGVyfHJvb3R8MzM4MDg3fGltYWdlL3BuZ3xoMGYvaGY3LzE0MDcwNjc0NzE4NzUwLnBuZ3xiMDUwZmMyMGMxODkxY2JkNTdhMjZmMWM1YWY1NWZmYTllMTg4Y2E1NGFjMjMzMjRkZjQ1YjA1ZjA3YTFjMThi"
-            />
-            <Card.Body>
-              <Card.Title>
-                Notebook ThinkPad X1 Carbon 9na Gen (14", Intel)
-              </Card.Title>
-              <Card.Text>$1.890.000</Card.Text>
-              <div className="buttons">
-                <NavLink to="/Detalle">
-                  <button className="botonvermas">Ver Más</button>
-                </NavLink>
-              </div>
-            </Card.Body>
-          </Card>
-        </Container>
-      </div>
+    <div className="homeContainerSuperior">
+      <Container>
+      <div className="homeContainer">
+       
+    {
+      productos.map((p) => {
+        return(
+          <div  className="cards"   key={p.ID} >
+            
+          
+             
+              <Card className=""
+              style={{
+                height: "55vh"
+                }}
+              >
+                
+                <Card.Img key={p.ID}
+                  variant="top"
+                  src={p.IMG}
+                  
+                />
+                
+                <Card.Body>
+                  <Card.Text >{p.MARCA}</Card.Text>
+                  <Card.Title >
+                    {p.TITULO}
+                  </Card.Title>
+                  
+                  <Card.Text>$ {p.PRECIO}</Card.Text>
+                  <div className="buttons">
+                    
+                    <button className="botonvermas" onClick={irDetalle}>Ver Más</button>
+                    
+                  </div>
+                </Card.Body>
+              </Card>
+            
+
+        </div>
+        )
+        
+        
+      
+      })
+    }
+    </div>
+    </Container>
     </div>
   );
-}
+};
 
 export default Cards;
