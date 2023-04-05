2ec16f8c5b7eff0a0a75e75b3d5bd793a3757240
filipgonzo <109770351+filipgonzo@users.noreply.github.com>
2023-04-04 22:07:11 -0400

    Confeccion Mis Publicaciones

--- src/Views/MisPublicaciones.jsx
+++ src/Views/MisPublicaciones.jsx
@@ -1,7 +1,82 @@
 import React from "react";
+<<<<<<< Updated upstream
 
 const MisPublicaciones = () => {
   return <div>MisPublicaciones</div>;
+=======
+import SubMenuPriv from "../Components/SubMenupriv";
+import { Button } from "react-bootstrap";
+
+const MisPublicaciones = () => {
+  return (
+    <div>
+      <SubMenuPriv />
+      <div className="contenedorPublicaciones">
+        <div className="recuadro">
+          <div className="ïmagen">
+            <img
+              src="https://cdnx.jumpseller.com/bhagatty/image/28431938/resize/610/610?1665774046"
+              alt="notebook"
+            />
+          </div>
+          <div className="tabla">
+            <table>
+              <tr>
+                <td className="titulos"> Nombre </td>
+                <td className="titulos"> Descripcion</td>
+                <td className="titulos"> Precio </td>
+              </tr>
+              <tr>
+                <td> NOTEBOOK THINKPAD X1 CARBON 9NA GEN (14", INTEL) </td>
+                <td>NOTEBOOK THINKPAD X1 CARBON 9NA GEN (14", INTEL)</td>
+                <td> $ 5.000.000 </td>
+              </tr>
+            </table>
+          </div>
+          <div className="botones">
+            <div className="editar">
+              <Button variant="secondary" className="btn-publicar"
+                
+                /*style={{
+                  height: "5vh",
+                  width: "8vw",
+                  borderRadius: "30% 30% 30% 30%",
+                  backgroundColor: "rgb(86,209, 200)",
+                  color: "black",
+                  border: "1px rgb(86,209, 200) ",
+                }}*/
+              >
+                <strong> EDITAR </strong>
+              </Button>
+            </div>
+            <div className="eliminar">
+              <Button variant="outline-secondary" className="btn-cancelar"
+                
+                /* style={{
+                  height: "5vh",
+                  width: "8vw",
+                  borderRadius: "30% 30% 30% 30%",
+                  backgroundColor: "rgb(255,87, 87)",
+                  color: "black",
+                  border: "1px rgb(255,87, 87) ",
+                }} */
+              >
+                <strong> ELIMINAR </strong>
+              </Button>
+            </div>
+            <div className=""></div>
+          </div>
+        </div>
+
+        <div className="agregarPublicacion">
+          <div className="btnMas">
+            <strong>+</strong>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+>>>>>>> Stashed changes
 };
 
 export default MisPublicaciones;
